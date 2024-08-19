c5d0c2106bd3d41a0c1605fd0a664e9c90f53654
caio <caiothalles607@gmail.com>
2024-08-19 09:21:40 -0300

    feat: created the connection with the mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { fastify } from "fastify";
+import mongoose from "mongoose";
 
 const app = fastify({ logger: true });
 
 const processEnv = Number(process.env.PORT);
+
+mongoose.connect(
+  "mongodb+srv://mongo:12345@cluster0.lquvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 const start = async () => {
   try {
