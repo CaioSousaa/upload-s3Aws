18f0073cd09c136d372d94cd4f8f0af0932f9e46
caio <caiothalles607@gmail.com>
2024-08-20 20:17:10 -0300

    chore: add route for the GET method

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { createFactoryUpload } from "../modules/upload/factory/create-upload-factory";
 import { upload } from "../config/multer";
+import { findManyUploadFactory } from "../modules/upload/factory/find-many-upload-factory";
 
 const routes = Router();
 
 routes.post("/upload", upload.single("file"), (req, res) => {
   return createFactoryUpload().handle(req, res);
+});
+
+routes.get("/uploads", (req, res) => {
+  return findManyUploadFactory().handle(req, res);
 });
 
 export { routes };
