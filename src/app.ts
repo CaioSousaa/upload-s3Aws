99dd7616d2b9ccb2858897cb7e9857981c850b85
caio <caiothalles607@gmail.com>
2024-08-20 19:52:39 -0300

    chore: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import { routes } from "./routes/upload.routes";
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(routes);
