721b8e80857e68383dc6351d3add35db0632614d
caio <caiothalles607@gmail.com>
2024-08-20 19:51:31 -0300

    fix: modifying the multer to load the images into the aws bucket

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,20 +1,51 @@
 import multer from "multer";
 import path from "path";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
-    const uploadPath = path.resolve(__dirname, "..", "tmp", "uploads");
-    cb(null, uploadPath);
-  },
-
-  filename: (req, file, cb) => {
-    const time = new Date().getTime();
-    cb(null, `${time}_${file.originalname}`);
+const s3 = new S3Client({
+  region: process.env.AWS_DEFAULT_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_SECRET_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
+interface CustomFile extends Express.Multer.File {
+  key?: string;
+}
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (req, res, cb) => {
+      const uploadPath = path.resolve(__dirname, "..", "tmp", "uploads");
+      cb(null, uploadPath);
+    },
+
+    filename: (req, file: CustomFile, cb) => {
+      const time = new Date().getTime();
+
+      file.key = `${time}_${file.originalname}`;
+
+      cb(null, file.key);
+    },
+  }),
+  s3: multerS3({
+    s3: s3,
+    bucket: "uploadimagess3",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, cb) => {
+      const time = new Date().getTime();
+
+      const filename = `${time}_${file.originalname}`;
+
+      cb(null, filename);
+    },
+  }),
+};
+
 export const upload = multer({
-  storage: storage,
+  storage: storageTypes["s3"],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- src/modules/upload/controller/create-upload-controller.ts
+++ src/modules/upload/controller/create-upload-controller.ts
@@ -2,21 +2,27 @@ import { Request, Response } from "express";
 import { CreateUploadService } from "../service/create-upload";
 import { AppError } from "../../../error/AppError";
 
+interface CustomFile extends Express.Multer.File {
+  url?: string;
+  key?: string;
+  location?: string;
+}
+
 export class CreateUploadController {
   constructor(private createUploadService: CreateUploadService) {}
   async handle(req: Request, res: Response) {
-    const file = req.file;
+    const file = req.file as CustomFile;
 
     if (!file) {
       throw new AppError(401, "no files inserted");
     }
 
-    const { originalname: name, size, filename: key } = file;
+    const { originalname: name, size, key = "", location: url = "" } = file;
 
     const createUpload = await this.createUploadService.execute({
       name,
       size,
-      url: "",
+      url,
       key,
     });
 
