7a59ca68706c4c1faf20228625dfe029be237d3a
caio <caiothalles607@gmail.com>
2024-08-20 20:16:44 -0300

    chore: add feature to redeem all uploads

--- src/repositories/IUpload-repository.ts
+++ src/repositories/IUpload-repository.ts
@@ -2,4 +2,5 @@ import { Upload } from "../modules/upload/entitie/Upload";
 
 export interface IUploadRepositoy {
   create(upload: Upload): Promise<Upload>;
+  find(): Promise<Upload[]>;
 }
--- src/repositories/upload-repository.ts/prisma-upload-repositoriy.ts
+++ src/repositories/upload-repository.ts/prisma-upload-repositoriy.ts
@@ -1,9 +1,14 @@
 import { IUploadRepositoy } from "../IUpload-repository";
 import { prisma } from "../../database/prisma";
-import { v4 as uuid } from "uuid";
 import { Upload } from "../../modules/upload/entitie/Upload";
 
 export class PrismaUploadRepository implements IUploadRepositoy {
+  async find(): Promise<Upload[]> {
+    const findUploads = await prisma.upload.findMany();
+
+    return findUploads;
+  }
+
   async create({ name, created_at, key, size, url }: Upload): Promise<Upload> {
     const uploadData = await prisma.upload.create({
       data: {
