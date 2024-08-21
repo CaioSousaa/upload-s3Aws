d1cc19f440dfb97ae831b4be7df0823a8b6371d4
caio <caiothalles607@gmail.com>
2024-08-20 21:12:28 -0300

    chore: add feature to delete fetch an upload

--- src/repositories/IUpload-repository.ts
+++ src/repositories/IUpload-repository.ts
@@ -3,4 +3,6 @@ import { Upload } from "../modules/upload/entitie/Upload";
 export interface IUploadRepositoy {
   create(upload: Upload): Promise<Upload>;
   find(): Promise<Upload[]>;
+  delete(id: string): Promise<void>;
+  findUploadById(id: string): Promise<Upload | null>;
 }
--- src/repositories/upload-repository.ts/prisma-upload-repositoriy.ts
+++ src/repositories/upload-repository.ts/prisma-upload-repositoriy.ts
@@ -1,8 +1,31 @@
 import { IUploadRepositoy } from "../IUpload-repository";
 import { prisma } from "../../database/prisma";
 import { Upload } from "../../modules/upload/entitie/Upload";
+import { AppError } from "../../error/AppError";
 
 export class PrismaUploadRepository implements IUploadRepositoy {
+  async findUploadById(id: string): Promise<Upload> {
+    const upload = await prisma.upload.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!upload) {
+      throw new AppError(401, "upload not existing");
+    }
+
+    return upload;
+  }
+
+  async delete(id: string): Promise<void> {
+    await prisma.upload.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
   async find(): Promise<Upload[]> {
     const findUploads = await prisma.upload.findMany();
 
